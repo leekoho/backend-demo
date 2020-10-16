88897c73d1bc06d81643d8f5ce607e4d7f81b341
李嘉豪 <leekoho95@163.com>
2020-10-16 10:24:37 +0800

    perf(koa-compress): 更改kao-compress插件的配置, 关闭brotli压缩

--- configs/koa.middlewares.ts
+++ configs/koa.middlewares.ts
@@ -2,6 +2,8 @@ import Koa from 'koa'
 import logger from 'koa-logger'
 import bodyParser from 'koa-bodyparser'
 import compress from 'koa-compress'
+import zlib from 'zlib'
+import compressible from 'compressible'
 
 export const useMiddlewares = <T extends Koa>(app: T): T => {
   process.env.NODE_ENV !== 'test' && app.use(logger())
@@ -10,8 +12,17 @@ export const useMiddlewares = <T extends Koa>(app: T): T => {
 
   app.use(
     compress({
+      filter: content_type => compressible(content_type),
       // 阈值, 当超过这个值时开启压缩
       threshold: 2048,
+      gzip: {
+        flush: zlib.constants.Z_SYNC_FLUSH,
+      },
+      deflate: {
+        flush: zlib.constants.Z_SYNC_FLUSH,
+      },
+      // disable brotli
+      br: false,
     }),
   )
 
